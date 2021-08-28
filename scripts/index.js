7c84cc14dba78f4db9fec5bbde118e353b7a746d
Lhcfl <lhcfl.outlook.com>
2021-08-28 19:52:40 +0800

    修复了字数统计的显示问题

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,3 @@ require('../includes/generators/insight')(hexo);
 require('../includes/generators/tags')(hexo);
 require('../includes/helpers/site')(hexo);
 
-// Debug helper
-hexo.extend.helper.register('console', function() {
-    console.log(arguments)
-});
