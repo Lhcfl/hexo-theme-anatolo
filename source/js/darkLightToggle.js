7d28531d2b72a16e3b8f9d767df9811ae2e0069d
Lhcfl <lhcfl@outlook.com>
2023-10-01 20:32:06 +0800

    更好的暗色亮色模式

--- source/js/darkLightToggle.js
+++ source/js/darkLightToggle.js
@@ -19,108 +19,126 @@
 |*|
 \*/
 
-const docCookies = {
-  getItem: function (sKey) {
-    return (
-      decodeURIComponent(
-        document.cookie.replace(
-          new RegExp(
-            "(?:(?:^|.*;)\\s*" +
-              encodeURIComponent(sKey).replace(/[-.+*]/g, "\\$&") +
-              "\\s*\\=\\s*([^;]*).*$)|^.*$"
-          ),
-          "$1"
-        )
-      ) || null
-    );
-  },
-  setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
-    if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) {
-      return false;
-    }
-    var sExpires = "";
-    if (vEnd) {
-      switch (vEnd.constructor) {
-        case Number:
-          sExpires =
-            vEnd === Infinity
-              ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT"
-              : "; max-age=" + vEnd;
-          break;
-        case String:
-          sExpires = "; expires=" + vEnd;
-          break;
-        case Date:
-          sExpires = "; expires=" + vEnd.toUTCString();
-          break;
-      }
-    }
-    document.cookie =
-      encodeURIComponent(sKey) +
-      "=" +
-      encodeURIComponent(sValue) +
-      sExpires +
-      (sDomain ? "; domain=" + sDomain : "") +
-      (sPath ? "; path=" + sPath : "") +
-      (bSecure ? "; secure" : "");
-    return true;
-  },
-  removeItem: function (sKey, sPath, sDomain) {
-    if (!sKey || !this.hasItem(sKey)) {
-      return false;
-    }
-    document.cookie =
-      encodeURIComponent(sKey) +
-      "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" +
-      (sDomain ? "; domain=" + sDomain : "") +
-      (sPath ? "; path=" + sPath : "");
-    return true;
-  },
-  hasItem: function (sKey) {
-    return new RegExp(
-      "(?:^|;\\s*)" +
-        encodeURIComponent(sKey).replace(/[-.+*]/g, "\\$&") +
-        "\\s*\\="
-    ).test(document.cookie);
-  },
-  keys: /* optional method: you can safely remove it! */ function () {
-    var aKeys = document.cookie
-      .replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "")
-      .split(/\s*(?:\=[^;]*)?;\s*/);
-    for (var nIdx = 0; nIdx < aKeys.length; nIdx++) {
-      aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]);
-    }
-    return aKeys;
-  },
-};
+// const docCookies = {
+//   getItem: function (sKey) {
+//     return (
+//       decodeURIComponent(
+//         document.cookie.replace(
+//           new RegExp(
+//             "(?:(?:^|.*;)\\s*" +
+//               encodeURIComponent(sKey).replace(/[-.+*]/g, "\\$&") +
+//               "\\s*\\=\\s*([^;]*).*$)|^.*$"
+//           ),
+//           "$1"
+//         )
+//       ) || null
+//     );
+//   },
+//   setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+//     if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) {
+//       return false;
+//     }
+//     var sExpires = "";
+//     if (vEnd) {
+//       switch (vEnd.constructor) {
+//         case Number:
+//           sExpires =
+//             vEnd === Infinity
+//               ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT"
+//               : "; max-age=" + vEnd;
+//           break;
+//         case String:
+//           sExpires = "; expires=" + vEnd;
+//           break;
+//         case Date:
+//           sExpires = "; expires=" + vEnd.toUTCString();
+//           break;
+//       }
+//     }
+//     document.cookie =
+//       encodeURIComponent(sKey) +
+//       "=" +
+//       encodeURIComponent(sValue) +
+//       sExpires +
+//       (sDomain ? "; domain=" + sDomain : "") +
+//       (sPath ? "; path=" + sPath : "") +
+//       (bSecure ? "; secure" : "");
+//     return true;
+//   },
+//   removeItem: function (sKey, sPath, sDomain) {
+//     if (!sKey || !this.hasItem(sKey)) {
+//       return false;
+//     }
+//     document.cookie =
+//       encodeURIComponent(sKey) +
+//       "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" +
+//       (sDomain ? "; domain=" + sDomain : "") +
+//       (sPath ? "; path=" + sPath : "");
+//     return true;
+//   },
+//   hasItem: function (sKey) {
+//     return new RegExp(
+//       "(?:^|;\\s*)" +
+//         encodeURIComponent(sKey).replace(/[-.+*]/g, "\\$&") +
+//         "\\s*\\="
+//     ).test(document.cookie);
+//   },
+//   keys: /* optional method: you can safely remove it! */ function () {
+//     var aKeys = document.cookie
+//       .replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "")
+//       .split(/\s*(?:\=[^;]*)?;\s*/);
+//     for (var nIdx = 0; nIdx < aKeys.length; nIdx++) {
+//       aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]);
+//     }
+//     return aKeys;
+//   },
+// };
 
-(() => {
-  if (docCookies.getItem("darkmode")) {
-    if (document.getElementById("theme-default-mode")) {
-        document.getElementById("theme-default-mode").id = "theme-dark-mode";
-    }
-    if (document.getElementById("theme-light-mode")) {
-        document.getElementById("theme-light-mode").id = "theme-dark-mode";
-    }
+function loadStyles(url){
+	var link = document.createElement("link");
+  link.id = "themecss"
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = url;
+  var head = document.getElementsByTagName("head")[0];
+  head.appendChild(link);
+}
+
+const defaultTheme = document.getElementById("default-theme").getAttribute("data");
+
+const url_for = (url) => {
+  const root = document.getElementById("site_root_url").getAttribute("data");
+  if (url[0] !== '/') {
+    url = '/' + url;
   }
-})();
-
-function darkLightToggle() {
-  if (!docCookies.getItem("darkmode")) {
-    if (document.getElementById("theme-default-mode")) {
-        document.getElementById("theme-default-mode").id = "theme-dark-mode";
-    }
-    if (document.getElementById("theme-light-mode")) {
-        document.getElementById("theme-light-mode").id = "theme-dark-mode";
-    }
-    docCookies.setItem("darkmode", true);
+  if (root[root.length - 1] == '/') {
+    return root.slice(0, -1) + url;
   } else {
-    if (document.getElementById("theme-default-mode")) {
-        document.getElementById("theme-default-mode").id = "theme-light-mode";
-    }
-    if (document.getElementById("theme-dark-mode")) {
-        document.getElementById("theme-dark-mode").id = "theme-light-mode";
-    }
-    docCookies.removeItem("darkmode");
+    return root + url;
   }
 }
+
+function setTheme() {
+  if (document.getElementById('themecss')){
+    document.getElementById('themecss').remove();
+  }
+  if (localStorage["themeChanged"] && defaultTheme == "light") {
+    loadStyles(url_for("css/theme/dark.css"));
+  } else if (localStorage["themeChanged"] && defaultTheme == "dark") {
+    loadStyles(url_for("css/theme/light.css"));
+  } else {
+    loadStyles(url_for(`css/theme/${defaultTheme}.css`));
+  }
+}
+
+setTheme();
+
+function darkLightToggle() {
+  if (!localStorage["themeChanged"]) {
+    localStorage.setItem("themeChanged", true);
+  } else {
+    localStorage.removeItem("themeChanged");
+  }
+  setTheme();
+}
+
