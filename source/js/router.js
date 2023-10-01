01f61d44bd73273754f37eefcb048880ce8f8385
Lhcfl <lhcfl@outlook.com>
2023-10-02 07:54:20 +0800

    fix: 后退键无返回

--- source/js/router.js
+++ source/js/router.js
@@ -1,8 +1,12 @@
+history.scrollRestoration = "manual";
+
+let historyStates = {};
+
 function isThisSite(url) {
   return (url.startsWith('/') && !url.startsWith('//')) || 
-    url.startsWith(window.location.origin) ||
-    ('https:' + url).startsWith(window.location.origin) ||
-    ('http:' + url).startsWith(window.location.origin)
+    url.startsWith(url_for('/', true)) ||
+    ('https:' + url).startsWith(url_for('/', true)) ||
+    ('http:' + url).startsWith(url_for('/', true))
 }
 
 function makeLink() {
@@ -18,9 +22,102 @@ function makeLink() {
   }
 }
 
-function routeTo(url) {
+/**
+ * Change Page
+ * @param {async () => {body: string, title:string}} callback 
+ * @param {boolean} pushState 
+ */
+function replacePage(callback, pushState = true) {
+  const sidebarheight = document.getElementsByClassName('sidebar')[0].clientHeight - 40;
   let animated_ok = 0;
   let whenOK = () => {};
+  $(".main.animated").removeClass('fadeInDown');
+  $(".main.animated").addClass('fadeOutDown');
+  setTimeout(() => {
+    animated_ok = 1;
+    whenOK();
+  }, 250);
+
+  function renderPage(url, body, title, scrollY) {
+
+    $(".main.animated").removeClass('fadeOutDown');
+    $(".main.animated").addClass('fadeInDown');
+    historyStates[document.location.href] = {
+      url: document.location.href,
+      body: document.getElementsByTagName('main-outlet')[0].innerHTML,
+      title: document.title,
+      scrollY: window.scrollY,
+    }
+
+    document.getElementsByTagName('main-outlet')[0].innerHTML = body;
+
+    document.title = title;
+  
+    if (pushState) {
+      history.pushState({
+        time: new Date()
+      }, title, url);
+    }
+
+    historyStates[document.location.href] = {
+      url: document.location.href,
+      body,
+      title,
+    };
+  
+  
+    makeLink();
+    if (!scrollY) {
+      if (window.innerWidth > 960) {
+        window.scrollTo({
+          left: 0,
+          top: 0,
+          behavior: 'smooth'
+        });
+      } else {
+        if (
+          window.location.href === url_for('/', true) || 
+          window.location.href.slice(0, -1) === url_for('/', true)
+        ) {
+          window.scrollTo({
+            left: 0,
+            top: 0,
+            behavior: 'smooth'
+          });
+        } else {
+          window.scrollTo({
+            left: 0,
+            top: sidebarheight,
+            // behavior: 'smooth'
+          });
+        }
+      }
+    } else {
+      window.scrollTo({
+        left: 0,
+        top: scrollY,
+      });
+    }
+    
+    animated_ok = 0;
+    whenOK = () => {};    
+  }
+
+  callback().then(res => {
+    if (animated_ok) {
+      renderPage(res.url, res.body, res.title, res.scrollY);
+    } else {
+      whenOK = () => renderPage(res.url, res.body, res.title, res.scrollY);
+    }
+  }).catch(err => {
+    $(".main.animated").removeClass('fadeOutDown');
+    $(".main.animated").addClass('fadeInDown');
+    console.error(err);
+    alert(JSON.stringify(err));
+  });
+}
+
+function routeTo(url) {
   if (isThisSite(url)) {
     const splited = url.split('/');
     if (splited[splited.length - 1] && splited[splited.length - 1].startsWith('#')) {
@@ -36,59 +133,15 @@ function routeTo(url) {
       return;
     }
     if (url == window.location.href) return;
-    const sidebarheight = document.getElementsByClassName('sidebar')[0].clientHeight - 40;
-    $.ajax(url).then(res => {
-      // console.log(res);
-      function replacePage() {
-        const body = res.match(/<main-outlet>([\s\S]*)<\/main-outlet>/)[1];
-        const head = res.match(/<head>([\s\S]*)<\/head>/)[1];
-        const title = head.match(/<title>([\s\S]*)<\/title>/)[1];
-
-        document.getElementsByTagName('main-outlet')[0].innerHTML = body;
-        document.title = title;
-
-        history.pushState(null, null, url);
-        makeLink();
-        if (window.innerWidth > 960) {
-          window.scrollTo({
-            left: 0,
-            top: 0,
-            behavior: 'smooth'
-          });
-        } else {
-          if (
-            window.location.href === window.location.origin || 
-            window.location.href.slice(0, -1) === window.location.origin
-          ) {
-            window.scrollTo({
-              left: 0,
-              top: 0,
-              behavior: 'smooth'
-            });
-          } else {
-            window.scrollTo({
-              left: 0,
-              top: sidebarheight,
-              // behavior: 'smooth'
-            });
-          }
-        }
-        
-        animated_ok = 0;
-        whenOK = () => {};
-      }
-      if (animated_ok) {
-        replacePage();
-      } else {
-        whenOK = replacePage;
-      }
+    
+    replacePage(async () => {
+      res = await $.ajax(url);
+      const body = res.match(/<main-outlet>([\s\S]*)<\/main-outlet>/)[1];
+      const head = res.match(/<head>([\s\S]*)<\/head>/)[1];
+      const title = head.match(/<title>([\s\S]*)<\/title>/)[1];
+      return {url, body, title};
     });
-    $(".main.animated").removeClass('fadeInDown');
-    $(".main.animated").addClass('fadeOutDown');
-    setTimeout(() => {
-      animated_ok = 1;
-      whenOK();
-    }, 250);
+    
   } else {
     window.location.href = url;
   }
@@ -96,3 +149,24 @@ function routeTo(url) {
 
 makeLink();
 setInterval(() => makeLink(), 1000);
+
+function onPopState(event) {
+  event.preventDefault();
+
+  // console.log(event);
+  // console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
+
+  replacePage(async () => {
+    if (historyStates[document.location.href]) {
+      return historyStates[document.location.href];
+    } else {
+      res = await $.ajax(document.location.href);
+      const body = res.match(/<main-outlet>([\s\S]*)<\/main-outlet>/)[1];
+      const head = res.match(/<head>([\s\S]*)<\/head>/)[1];
+      const title = head.match(/<title>([\s\S]*)<\/title>/)[1];
+      return {url: document.location.href, body, title}
+    }
+  }, false);
+}
+
+window.addEventListener('popstate', onPopState, false);
