d5057efe813a0d0596f2061df68838f1d54b08f8
Lhcfl <lhcfl@outlook.com>
2024-06-13 20:28:08 +0800

    feat: add float btn

--- source/js/anatolo.js
+++ source/js/anatolo.js
@@ -174,3 +174,24 @@ class AnatoloSite {
 /** @type {import("eventemitter3").EventEmitter & AnatoloManager} */
 const Anatolo = new AnatoloManager();
 Anatolo.site = new AnatoloSite();
+
+jQuery.event.special.touchstart = {
+  setup: function (_, ns, handle) {
+    this.addEventListener('touchstart', handle, { passive: !ns.includes('noPreventDefault') });
+  },
+};
+jQuery.event.special.touchmove = {
+  setup: function (_, ns, handle) {
+    this.addEventListener('touchmove', handle, { passive: !ns.includes('noPreventDefault') });
+  },
+};
+jQuery.event.special.wheel = {
+  setup: function (_, ns, handle) {
+    this.addEventListener('wheel', handle, { passive: true });
+  },
+};
+jQuery.event.special.mousewheel = {
+  setup: function (_, ns, handle) {
+    this.addEventListener('mousewheel', handle, { passive: true });
+  },
+};
