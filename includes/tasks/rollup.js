cdffb9cb13fe0a1ff9bd8f9fe41135dabb396056
Lhcfl <lhcfl@outlook.com>
2024-09-04 15:54:45 +0800

    change main font

--- includes/tasks/rollup.js
+++ includes/tasks/rollup.js
@@ -2,7 +2,9 @@ const cp = require('child_process');
 
 /** @param {import("hexo")} hexo */
 module.exports = function (hexo) {
-  if (hexo.env?.cmd?.startsWith('n')) { return; }
+  if (hexo.env?.cmd?.startsWith('n')) {
+    return;
+  }
   hexo.log.info('building js');
   cp.execSync('yarn build', { cwd: './themes/Anatolo', stdio: 'inherit' });
   hexo.log.info('build successful!');
