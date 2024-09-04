686811bf0d607f9bf027abc762352c2b53042eae
Lhcfl <lhcfl@outlook.com>
2024-09-04 14:33:38 +0800

    update rollup.js: disable rollup when hexo new

--- includes/tasks/rollup.js
+++ includes/tasks/rollup.js
@@ -2,6 +2,7 @@ const cp = require('child_process');
 
 /** @param {import("hexo")} hexo */
 module.exports = function (hexo) {
+  if (hexo.env?.cmd?.startsWith('n')) { return; }
   hexo.log.info('building js');
   cp.execSync('yarn build', { cwd: './themes/Anatolo', stdio: 'inherit' });
   hexo.log.info('build successful!');
