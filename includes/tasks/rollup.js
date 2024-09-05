ccbd7255551528e5e76bc904f60600303e0269f8
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-09-05 23:12:42 +0800

    fix: :art: replace more yarn

--- includes/tasks/rollup.js
+++ includes/tasks/rollup.js
@@ -6,6 +6,6 @@ module.exports = function (hexo) {
     return;
   }
   hexo.log.info('building js');
-  cp.execSync('yarn build', { cwd: './themes/Anatolo', stdio: 'inherit' });
+  cp.execSync('pnpm build', { cwd: './themes/Anatolo', stdio: 'inherit' });
   hexo.log.info('build successful!');
 };
