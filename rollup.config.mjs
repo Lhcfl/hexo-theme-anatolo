c594d297b31612d9c0defa7194fd96a55fc48d56
Lhcfl <lhcfl@outlook.com>
2024-09-07 14:58:10 +0800

    Revert "perf: :zap: properly configure minify options"
    This reverts commit fbd95010d47efef86a87a3bf120c82202b6fa17d.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig([
       swc.rollup({
         minify: true,
         jsc: {
+          target: 'esnext',
           baseUrl: pathResolve('./'),
           paths: {
             '@/*': ['./src/*'],
