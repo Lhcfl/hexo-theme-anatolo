4f9a50c16c5e94619d411c624aab92fa7c994886
北雁云依 <14120445+BeiyanYunyi@users.noreply.github.com>
2024-09-08 19:46:13 +0800

    perf: ⚡ properly configure minify options (#27)
    * perf: :zap: properly configure minify options
    
    * refactor: :fire: no need to resolve
    
    * fix: :bug:

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { Anatolo } from './anatolo/anatolo';
 import * as Utils from './utils/main';
+import FloatBtn from './components/float-btn';
 
 (window as any).Anatolo = Anatolo;
 (window as any).Utils = Utils;
 
-import FloatBtn from './components/float-btn';
 new FloatBtn();
