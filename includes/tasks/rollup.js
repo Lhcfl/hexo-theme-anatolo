147f012cad7583e8d35715e76cc295f92f197b39
linca <lhcfl@outlook.com>
2026-08-23 15:50:44 +0800

    fix(ci): update ci

--- includes/tasks/rollup.js
+++ includes/tasks/rollup.js
@@ -2,15 +2,18 @@ const cp = require('child_process');
 
 /** @param {import("hexo")} hexo */
 module.exports = function (hexo) {
+  const path = require("node:path");
+  const cwd = path.join(module.path, "../../")
+
   if (hexo.env?.cmd?.startsWith('n')) {
     return;
   }
   if (hexo.env?.cmd === 's' || hexo.env?.cmd === 'server') {
     hexo.log.info('Starting js watch changer...');
-    cp.exec('pnpm watch', { cwd: './themes/Anatolo', stdio: 'inherit' });
+    cp.exec('pnpm watch', { cwd, stdio: 'inherit' });
   } else {
     hexo.log.info('Building js...');
-    cp.execSync('pnpm build', { cwd: './themes/Anatolo', stdio: 'inherit' });
+    cp.execSync('pnpm build', { cwd, stdio: 'inherit' });
     hexo.log.info('Build successful!');
   }
 };
